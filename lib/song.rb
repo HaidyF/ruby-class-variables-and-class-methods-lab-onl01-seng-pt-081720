ddad555b02d70d2f472b66f19f12dc8b8a364a39
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-05 03:43:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ attr_accessor :name, :artist, :genre
 @@artists=[]
 @@genres=[]
 
-def initialize(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
@@ -14,18 +14,16 @@ def initialize(name, artist, genre)
     @@song_count +=1
   end
 
-def self.count
-  @@song_count
-end
+  def self.count
+    @@song_count
+  end
 
-def self.artists
-  @@artists
-end
+  def self.artists
+    @@artists
+  end
 
-def self.genres
-  @@genres
-end
-
-
+  def self.genres
+    @@genres
+  end
 
 end
