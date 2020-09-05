67b60ca2cf2f62f475c17b7952b5ef579fc9b41b
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-05 03:52:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,16 +27,16 @@ attr_accessor :name, :artist, :genre
   end
 
   def self.genre_count
-    genre_count = {}
-    @@genre.each do |genre|
-      if genre_count[genre]
-      genre_count[genre] += 1
-      else
-      genre_count[genre] = 1
+      genre_count = {}
+      @@genres.each do |genre|
+        if genre_count[genre]
+        genre_count[genre] += 1
+        else
+        genre_count[genre] = 1
+        end
       end
+      genre_count
     end
-    genre_count
-  end
 
 
     def self.artist_count
