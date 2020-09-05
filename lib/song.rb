65a0c4fdba70b41bc79b9f676565ae977c572163
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-05 03:42:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,22 @@
 class Song
 attr_accessor :name, :artist, :genre
 
-def count
-  self.count
+@@song_count= 0
+@@artists=[]
+@@genres=[]
+
+def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
+  end
+
+def self.count
+  @@song_count
 end
+
 
 end
